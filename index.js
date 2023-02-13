ea83f06705373cfb66fa380af05893ff2d16a4dc
Josipa Šaravanja <josipa.saravanja06@gmail.com>
2023-02-13 20:26:46 +0100

    2nd task

--- index.js
+++ index.js
@@ -1,34 +1,72 @@
 //1. Zdk
-class Person{
-    constructor(firstName, lastName, height){
-        this.firstName=firstName;
-        this.lastName=lastName;
-        this.height=height;
+function zdk1(){
+    class Person{
+        constructor(firstName, lastName, height){
+            this.firstName=firstName;
+            this.lastName=lastName;
+            this.height=height;
+        }
     }
-}
 
-const people=[];
-let loop=true;
-while(loop){
-    const firstName=prompt("Ime:");
-    const lastName=prompt("Prezime:");
-    const height=Number(prompt("Visina:"));
+    const people=[];
+    let loop=true;
+    while(loop){
+        const firstName=prompt("Ime:");
+        const lastName=prompt("Prezime:");
+        const height=Number(prompt("Visina:"));
 
-    people.push(new Person(firstName, lastName, height));
-    const answer=prompt(`Želite li završiti unos (DA/NE)?`);
-    answer===`DA` ? loop=false: false;
-}
-let avg=0;
-for(const person of people){
-    avg+=person.height;
-}
-avg=avg/people.length;
-let heightDifference=0;
-let personDifference;
-for(const person of people){
-    if(Math.abs(person.height-avg)>heightDifference){
-        heightDifference=Math.abs(person.height-avg);
-        personDifference=person;
+        people.push(new Person(firstName, lastName, height));
+        const answer=prompt(`Želite li završiti unos (DA/NE)?`);
+        answer===`DA` ? loop=false: false;
     }
+    let avg=0;
+    for(const person of people){
+        avg+=person.height;
+    }
+    avg=avg/people.length;
+    let heightDifference=0;
+    let personDifference;
+    for(const person of people){
+        if(Math.abs(person.height-avg)>heightDifference){
+            heightDifference=Math.abs(person.height-avg);
+            personDifference=person;
+        }
+    }
+    console.log(personDifference);
 }
-console.log(personDifference);
+//2. Zdk
+function zdk2(){
+    class Person{
+        constructor(firstName, lastName, eyeColor, haircolor, height){
+            this.firstName=firstName;
+            this.lastName=lastName;
+            this.eyeColor=eyeColor;
+            this.hairColor=haircolor;
+            this.height=height;
+        }
+    }
+
+    const num=Number(prompt(`Broj osoba koje elite uniti:`));
+    const people=[]
+    for(i=0; i<num; i++){
+        const firstName=prompt("Ime:");
+        const lastName=prompt("Prezime:");
+        const eyeColor=prompt("Boja očiju:");
+        const hairColor=prompt("Boja kose:");
+        const height=Number(prompt("Visina:"));
+
+        people.push(new Person(firstName, lastName, eyeColor, hairColor, height));
+    }
+
+    people.sort((x, y) => {
+        if (x.height >= y.height)
+            return 1;
+        else
+            return -1;
+    })
+
+    const overMedian = people.slice(people.length/2, people.length);
+    overMedian.forEach(person=> {
+        person.eyeColor===person.hairColor? console.log(person):false;
+    });
+}
